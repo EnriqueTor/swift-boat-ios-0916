c0a63fd386701cc82cc475adac4a65b3c7c1b7f0
EnriqueTor <etorrendell@gmail.com>
2016-09-28 11:09:52 -0400

    Complete Problems

---
+++ swift-boat/boat.swift
@@ -0,0 +1,33 @@
+//
+//  boat.swift
+//  swift-boat
+//
+//  Created by Enrique Torrendell on 9/28/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Boat {
+    
+    let name : String
+    var sailors : [String]
+    var maxSpeedKnots : Double
+    var speedKnots : Double
+    
+    init(name:String,sailors:[String],maxSpeedKnots:Double) {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+        speedKnots = 0.0
+    }
+    
+    init(name:String,maxSpeedKnots:Double) {
+        self.name = name
+        self.sailors = []
+        self.maxSpeedKnots = maxSpeedKnots
+        speedKnots = 0.0
+    }
+}
